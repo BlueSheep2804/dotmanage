15e11af2a0f41495e33cc7835aa8c811fcc7f4a7
BlueSheep2804 <48321445+BlueSheep2804@users.noreply.github.com>
2020-10-20 22:31:10 +0900

    [ADD]ファイルのシンボリックリンクを配置するように

--- dotmanage.py
+++ dotmanage.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import sys
+from pathlib import Path
 
 
 def _check_git():
@@ -32,12 +33,26 @@ def init():
 def link():
     print('Create symbolic link.')
 
+    home_files = Path(os.environ['HOME'])
+
     print('Target user home directory')
-    print(f'{os.environ["HOME"]}')
+    print(f'{str(home_files)}')
     print('Are you sure?')
     inp = input('[y/N]>> ')
     if inp != 'y':
         sys.exit(1)
+
+    files = Path('./home/').glob('**/*')
+
+    for f in files:  # TODO: ファイル存在時の例外をキャッチする
+        print(f.resolve())
+        print(f'{str(home_files)}/{str(f)[5:]}')
+        if f.is_dir():
+            os.makedirs(f'{str(home_files)}/{f.name}', exist_ok=True)
+        elif f.is_file():
+            pass
+            os.symlink(f.resolve(), f'{str(home_files)}/{str(f)[5:]}')
+
 
 
 print(sys.argv)
