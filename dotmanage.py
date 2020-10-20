de6d062f8fc6e10ae15cd0f83bbb0e687636117f
BlueSheep2804 <48321445+BlueSheep2804@users.noreply.github.com>
2020-10-20 20:52:07 +0900

    [ADD]gitが存在するか否かを判定する関数を作った

--- dotmanage.py
+++ dotmanage.py
@@ -1,4 +1,24 @@
-import os
+import subprocess as subproc
 import sys
 
+
+def _check_git():
+    if subproc.call('which git', shell=True) == 0:
+        return True
+    else:
+        return False
+
+
+def init():
+    if not _check_git():
+        return False
+
+    print('Your dotfiles remote repository')
+    print('e.g. git@github.com:BlueSheep2804/dotfiles')
+    remote_uri = input('>> ')
+    if not remote_uri:
+        sys.exit(1)
+
+
 print(sys.argv)
+init()
